05cea6d4980f92dfb11f170fb4bf635b83c3b380
Lirjeta <lirjeta@Lirjetas-MacBook-Pro.local>
2022-03-06 19:16:05 -0500

    Fixed styling to include all pages

--- src/main/java/com/example/groupproject/web/CanadaCensusController.java
+++ src/main/java/com/example/groupproject/web/CanadaCensusController.java
@@ -1,6 +1,7 @@
 package com.example.groupproject.web;
 
 import com.example.groupproject.bean.GeographicArea;
+import com.example.groupproject.handler.AgeHandler;
 import com.example.groupproject.handler.GeographicAreaClassficationListHandler;
 import com.example.groupproject.handler.GeographicAreaHandler;
 
@@ -11,18 +12,21 @@ import java.io.IOException;
 import java.util.List;
 
 import static com.example.groupproject.handler.GeographicAreaHandler.geoAreaDetail;
+import static com.example.groupproject.handler.AgeHandler.age;
 import static com.example.groupproject.handler.GeographicAreaHandler.totalPopulation;
 
 @WebServlet(name = "CanadaCensusController", value = "/CanadaCensusController")
 public class CanadaCensusController extends HttpServlet {
 
     private GeographicAreaHandler geographicAreaHandler;
+    private AgeHandler ageHandler;
     private GeographicAreaClassficationListHandler geographicAreaClassficationListHandler;
     boolean result = false;
 
     //setting constants for forwarding the request/response
     final String Census_LOGIN = "/login.jsp";
     final String Census_HOME = "/home.jsp";
+    final String Census_AGE = "/AgeList.jsp";
     final String Census_GEOGRAPHICAREACLASSIFICATIONLIST = "/GeographicAreaClassificationList.jsp";
     final String Census_INDIVIDUALGEOGRPHICAREADETAIL = "/IndividualGeographicAreaDetailsPage.jsp";
 
@@ -30,6 +34,7 @@ public class CanadaCensusController extends HttpServlet {
         super();
         //creating an instance of GeographicAreaHandler
         geographicAreaHandler = new GeographicAreaHandler();
+        ageHandler = new AgeHandler();
         geographicAreaClassficationListHandler = new GeographicAreaClassficationListHandler();
     }
 
@@ -48,12 +53,11 @@ public class CanadaCensusController extends HttpServlet {
 
         //if the request is from login page then check username and password to give access
         //username and password are the ones used for accessing database
-        if (pageName.equals("login")){
-            if ( request.getParameter("userName").equals("root") &&
-                    request.getParameter("password").equals("Sara3142")){
+        if (pageName.equals("login")) {
+            if (request.getParameter("userName").equals("root") &&
+                request.getParameter("password").equals("denlir9899")) {
                 forward = Census_HOME;
-            }
-            else {
+            } else {
                 message = "User does not exist";
                 request.setAttribute("message", message);
                 forward = Census_LOGIN;
@@ -64,7 +68,7 @@ public class CanadaCensusController extends HttpServlet {
         if (geographicAreaHandler != null) {
             if (pageName.equals("geographicAreaDetail")) {
                 result = geographicAreaHandler.findGeographicAreaById(
-                        Integer.parseInt(request.getParameter("geographicAreaID")));
+                    Integer.parseInt(request.getParameter("geographicAreaID")));
                 if (result == true) {
                     request.setAttribute("geoAreaObj", geoAreaDetail);
                     request.setAttribute("totalPopulation", totalPopulation);
@@ -80,13 +84,29 @@ public class CanadaCensusController extends HttpServlet {
         if (geographicAreaClassficationListHandler != null) {
             if (pageName.equals("GeographicAreaClassificationList")) {
                 List<GeographicArea> geographicAreaList = geographicAreaClassficationListHandler.findGeographicAreaByLevel(
-                                                          Integer.parseInt(request.getParameter("level")));
+                    Integer.parseInt(request.getParameter("level")));
 
                 request.setAttribute("geoAreaObjList", geographicAreaList);
 
                 forward = Census_GEOGRAPHICAREACLASSIFICATIONLIST;
             }
         }
+
+        //if the request is from ageList then fetch the detail for male and female population and pass it to jsp page
+        if (ageHandler != null) {
+            if (pageName.equals("agePage")) {
+                result = ageHandler.findMaleFemaleByCensusYear(
+                    Integer.parseInt(request.getParameter("censusYear")));
+                if (result == true) {
+                    request.setAttribute("ageObj", age);
+                } else {
+                    message = "This census year is not in our database";
+                    request.setAttribute("message", message);
+                }
+                forward = Census_AGE;
+            }
+        }
+
         RequestDispatcher view = request.getRequestDispatcher(forward);
         view.forward(request, response);
     }
